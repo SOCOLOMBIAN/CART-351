5bd102079594b81bf584e825477effc3547ec8ef
SOCOLOMBIAN <sophie7304@outlook.com>
2025-09-24 22:17:37 -0400

    updating the last points and finishing the details

--- Sanchez_Sophie_Exercise_one/ex-one.py
+++ Sanchez_Sophie_Exercise_one/ex-one.py
@@ -32,7 +32,60 @@ print(type(responseData))
 for item in responseData:
     print(item)
 
-# a dict
-print(type(responseData)) 
-#get the keys
-print(responseData.keys())
+#12
+print("type of item:", type(item))
+
+#13 the type of key associated are dict_keys(['uid', 'aqi', 'time', 'station'])
+print (item.keys())
+
+#14 name of the station in responsedata, assign the name of latitude,longitude, air quality and uid and print the coordinates 
+for station in responseData:
+    latitude= station ['station'] ['geo'] [0]
+    longitude= station ['station'] ['geo'] [1]
+    aqui= station ['aqi']
+    uid= station ['uid']
+    print (f"Air Quality: {aqui}")
+    print (f" uid: {uid}")
+    print(f"lat:{latitude} log:{longitude}") 
+
+#part 6
+
+#1 /feed/5460/?token=79809a7dd45c290747d082657bd73fcaedfdd200
+ 
+url_feed = "https://api.waqi.info/feed/@5460"
+response_feed = requests.get(url_feed, params={"token": token})
+results_feed = response_feed.json()
+print(results_feed)
+
+#2  name the expression to acces to the content of the data 
+response_data_feed= results_feed ['data']
+
+#3 the type of item: <class 'dict'>
+print("type of item:", type(response_data_feed))
+
+#4 for loop to iterate trough response_data_feed
+for item in response_data_feed:
+    print(f"key: {item}, value: {response_data_feed[item]}") #key and value
+
+#5 expression to acces the field of aqi and dominentpol
+# aqi: Air Quality Index 
+# dominentpol: The pollutant 
+iaqi_value= response_data_feed ['iaqi']
+dominentpol_value= response_data_feed ['dominentpol']
+
+print(f"iaqui: {iaqi_value}")
+print(f"dominetpol: {dominentpol_value}")
+
+#6 acces to the iaqi fiel
+iaqi_value= response_data_feed ['iaqi']
+
+print("type of item:", type(iaqi_value))
+print (iaqi_value.keys())
+
+
+print (f"value for dominant pollutant: {dominentpol_value}")
+
+for pollutant_item in response_data_feed["iaqi"]:
+    print(pollutant_item)
+
+#7 
