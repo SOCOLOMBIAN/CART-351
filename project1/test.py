ae7cf1234a89fd944682d7312752b18a57d50dd3
SOCOLOMBIAN <sophie7304@outlook.com>
2025-10-02 14:12:54 -0400

    trying to get the informtion from the cities

--- project1/test.py
+++ project1/test.py
@@ -1,1 +1,14 @@
-print("hello i am running python")
+#import the request for the weather
+import requests
+
+city= imput("enter a city name: ")
+
+#write the api key for the open weather
+api_key="629f1cc3c63fee4339bef39db763aff1"
+
+#get the website url 
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+response= requests.get(url)
+data= response.json()
+print(data)
