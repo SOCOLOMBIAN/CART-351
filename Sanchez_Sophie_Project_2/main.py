26844bb6e81c22a8f55578a3c3c939e41b71188f
SOCOLOMBIAN <sophie7304@outlook.com>
2025-10-30 10:12:24 -0400

    added on the index html

--- Sanchez_Sophie_Project_2/main.py
+++ Sanchez_Sophie_Project_2/main.py
@@ -2,7 +2,10 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app route("/")
+@app.route("/")
 def index():
+    return render_template("index.html", 
+                           user={"username":"sophie"}
+    )
 
-app.run()
+app.run(debug=True)
