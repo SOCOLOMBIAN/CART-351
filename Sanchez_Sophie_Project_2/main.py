0b32f5fd62f4a01d289c6a740801519c81ae5c62
SOCOLOMBIAN <sophie7304@outlook.com>
2025-11-01 15:47:53 -0400

    continue with the projct

--- Sanchez_Sophie_Project_2/main.py
+++ Sanchez_Sophie_Project_2/main.py
@@ -7,5 +7,9 @@ def index():
     return render_template("index.html", 
                            user={"username":"sophie"}
     )
+    
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 app.run(debug=True)
