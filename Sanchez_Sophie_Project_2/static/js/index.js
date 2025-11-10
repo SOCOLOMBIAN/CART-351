a248203b7f6903531edb80a396ca2858080417a8
SOCOLOMBIAN <sophie7304@outlook.com>
2025-11-10 10:08:43 -0500

    added to remove the naav bar

--- Sanchez_Sophie_Project_2/static/js/index.js
+++ Sanchez_Sophie_Project_2/static/js/index.js
@@ -14,19 +14,27 @@ try{
         readingsCount.textContent = data.readings.length;
 
         readingsContainer.innerHTML= '';
-   
-        
 
+        //display the readings
+        data.readings.forEach(reading => {
+            const readingCard = document.createElement('div');
+            readingCard.className='reading-card';
 
+            readingCard.innerHTML= `
+            <h3> ${reading.name} </h3>
+            <div class= "card-name"> ${reading.cardName} </div>
+            <p class="question"><strong>Question:</strong> ${reading.question}</p>
+            <p class="question"><strong>Birth Month:</strong> ${reading.month}</p>
+        `;
 
+        readingsContainer.appendChild(readingCard);
+
+        });
+    } else {
+        readings.textContent= '0';
+        readingsContainer.innerHTML= readingsContainer.innerHTML = '<p style="color: rgba(212, 191, 255, 0.7); text-align: center; grid-column: 1/-1;">No readings. Be the first!</p>';
     }
-
+} catch (error) {
+    console.error('Error fetching readings:', error);
 }
-
-
-
-
-
-
-}
-)
+});
